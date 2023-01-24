1eefe1354a03b7281b0daeb3bd1a716a9174ee32
Hapajik <chubaaleex3@gmail.com>
2023-01-24 20:34:09 +0300

    upd form

--- WFormApp9/FormSotr.Designer.cs
+++ WFormApp9/FormSotr.Designer.cs
@@ -30,12 +30,19 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(655, 415);
+            this.button1.Location = new System.Drawing.Point(655, 466);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 23);
             this.button1.TabIndex = 1;
@@ -50,18 +57,91 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(776, 279);
             this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
             // 
-            // Form2
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ФИО мастера";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(397, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Должность";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 348);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(291, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(468, 348);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(291, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(9, 391);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Изменить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(278, 391);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(238, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Отмена";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(550, 391);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(238, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Добавить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // FormSotr
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 501);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
-            this.Name = "Form2";
+            this.Name = "FormSotr";
             this.Text = "Form2";
+            this.Activated += new System.EventHandler(this.FormSotr_Activated);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +149,12 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- WFormApp9/FormSotr.cs
+++ WFormApp9/FormSotr.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WFormApp9
 {
     public partial class FormSotr : Form
     {
+        string nmas;
         public FormSotr()
         {
             InitializeComponent();
@@ -20,6 +22,94 @@ namespace WFormApp9
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void FormSotr_Activated(object sender, EventArgs e)
+        {
+            SqlConnection connection1 = new SqlConnection(Data.Glob_connection_string); connection1.Open();
+
+            string SQL = "Select n_mast, fio, dolg from MASTER";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(SQL, connection1);
+            DataTable tb = new DataTable();
+            adapter.Fill(tb);
+
+            dataGridView1.Refresh();
+            dataGridView1.DataSource = tb;
+            dataGridView1.Columns[0].HeaderText = "№ мастера";
+            dataGridView1.Columns[0].Width = 100;
+            connection1.Close();
+
+        }
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            {
+                textBox1.Text = dataGridView1[1, dataGridView1.CurrentRow.Index].Value.ToString();
+                textBox2.Text = dataGridView1[2, dataGridView1.CurrentRow.Index].Value.ToString();
+                nmas = dataGridView1[0, dataGridView1.CurrentRow.Index].Value.ToString();
+                button4.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection1 = new SqlConnection(Data.Glob_connection_string);
+            connection1.Open();
+
+            string SQL_izm = "UPDATE MASTER set fio=N'" + textBox1.Text +
+                            "', dolg=N'" + textBox2.Text + "' WHERE n_mast=" + nmas;
+
+
+            SqlCommand command1 = new SqlCommand(SQL_izm, connection1);
+            SqlDataReader dr = command1.ExecuteReader();
+            connection1.Close();
+            MessageBox.Show("Данные сохранены");
+            this.Activate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            button4.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string SQL_dob = "SELECT max(n_mast) as max FROM MASTER";
+
+            SqlConnection connection1 = new SqlConnection(Data.Glob_connection_string);
+            connection1.Open();
+
+            SqlCommand command1 = new SqlCommand(SQL_dob, connection1);
+            SqlDataReader dr = command1.ExecuteReader();
+            string max = "";
+            int max2 = 0;
+            while (dr.Read())
+            {
+                max = string.Format("{0}", dr["max"]);
+            }
+            dr.Close();
+            connection1.Close();
+            if (max == "") { max2 = 1; }
+            else { max2 = Convert.ToInt32(max) + 1; }
+            max = Convert.ToString(max2);
+
+            SQL_dob = "INSERT INTO MASTER(n_mast,fio,dolg) values (" + max + ", N'" + textBox1.Text +
+                "', N'" + textBox2.Text + "')";
+
+            connection1 = new SqlConnection(Data.Glob_connection_string);
+            connection1.Open();
+
+            command1 = new SqlCommand(SQL_dob, connection1);
+            dr = command1.ExecuteReader();
+            dr.Close();
+            connection1.Close();
+            MessageBox.Show("Данные сохранены");
+            this.Activate();
+
         }
     }
 }
